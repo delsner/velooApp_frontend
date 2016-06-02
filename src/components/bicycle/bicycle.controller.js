4447fcd98aba94aa903ea9566e93ab60a63fecdc
Constantin Mense <constantin.mense@tum.de>
2016-06-02 22:30:44 +0200

    create bicycle view tpl

--- src/components/bicycle/bicycle.controller.js
+++ src/components/bicycle/bicycle.controller.js
@@ -5,9 +5,16 @@
         .module('velooAngular')
         .controller('bicycleCtrl', bicycleCtrl);
 
-    function bicycleCtrl($scope) {
+    function bicycleCtrl($scope, uiGmapGoogleMapApi) {
         var vm = this;
 
+        vm.map = {
+            center: {
+                latitude: 48.137,
+                longitude: 11.577
+            },
+            zoom: 12
+        };
     }
 })(angular);
 
